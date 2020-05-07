76c60613b3897bf745b250648bf65406f1bd24ce
Adiel Cardoso <62920887+AdielCardosoDev@users.noreply.github.com>
2020-05-06 21:42:33 -0300

    Reparos de Felx

--- placargamer/App.js
+++ placargamer/App.js
@@ -120,11 +120,12 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#212121',
-    alignItems: 'center',    
+    alignItems: 'center',
+    justifyContent:'center'    
   },
   img:{
-    width:200,
-    height:200,
+    width:100,
+    height:100,
     marginTop:13,
   },
   title:{
@@ -246,8 +247,8 @@ const styles = StyleSheet.create({
     color:'#fff',
   },
   close:{
-    right:-155,
-    bottom:270
+    right:-140,
+    bottom:265
   }
   
 
